1ef6e3afd6c07687a144deee0ba51348a3cd3595
RZOK <47612683+RandomZachOfKindness@users.noreply.github.com>
2023-03-11 12:49:57 -0500

    oops

--- Content/Items/ProjectorLenses/ShimmeringGlass.cs
+++ Content/Items/ProjectorLenses/ShimmeringGlass.cs
@@ -1,3 +1,6 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Radiance.Content.Items.BaseItems;
 using Radiance.Core;
 using Terraria;
 using Terraria.ID;
@@ -24,6 +27,24 @@ namespace Radiance.Content.Items.ProjectorLenses
             Item.maxStack = 999;
             Item.value = Item.sellPrice(0, 0, 4);
             Item.rare = ItemRarityID.Blue;
+        }
+        Vector2 offset = Vector2.Zero;
+        public override bool PreDrawInWorld(SpriteBatch spriteBatch, Color lightColor, Color alphaColor, ref float rotation, ref float scale, int whoAmI)
+        {
+            Texture2D tex = ModContent.Request<Texture2D>(Texture).Value;
+            foreach (Item item in Main.item)
+            {
+                if (item.active && item.ModItem is BaseContainer container && item.Distance(Item.Center) < 100)
+                {
+                    float adjustedDist = (100f - item.Distance(Item.Center)) / 18;
+                    if (Main.GameUpdateCount % 4 == 0)
+                        offset = Main.rand.NextVector2Circular(-adjustedDist, adjustedDist);
+                    spriteBatch.Draw(tex, Item.Center + offset - Main.screenPosition, null, Color.White * 0.3f, rotation, tex.Size() / 2, 1, SpriteEffects.None, 0);
+                    spriteBatch.Draw(tex, Item.Center + -offset - Main.screenPosition, null, Color.White * 0.3f, rotation, tex.Size() / 2, 1, SpriteEffects.None, 0);
+                }
+            }
+            spriteBatch.Draw(tex, Item.Center - Main.screenPosition, null, Color.White * 0.9f, rotation, tex.Size() / 2, 1, SpriteEffects.None, 0);
+            return false;
         }
     }
 }
