9ffe96b9df3c7d7b791daa449e7f34d23779cd40
Zach <47612683+RZOK@users.noreply.github.com>
2024-12-02 16:58:22 -0500

    small stuff

--- Content/Items/Materials/PetrifiedCrystal.cs
+++ Content/Items/Materials/PetrifiedCrystal.cs
@@ -13,7 +13,7 @@ namespace Radiance.Content.Items.Materials
         {
             Item.width = 20;
             Item.height = 20;
-            Item.maxStack = 9999;
+            Item.maxStack = Item.CommonMaxStack;
             Item.value = Item.sellPrice(0, 0, 0, 50);
             Item.rare = ItemRarityID.White;
         }
