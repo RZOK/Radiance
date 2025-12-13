32fec8906d9016fb9dfa3812e0265093e9410a2d
Zach <47612683+RZOK@users.noreply.github.com>
2025-12-13 01:13:06 -0500

    gleaming whetstone entry

---
+++ Content/EncycloradiaEntries/Instruments/GleamingWhetstoneEntry.cs
@@ -0,0 +1,27 @@
+﻿using Radiance.Content.Items.Accessories;
+using Radiance.Core.Encycloradia;
+using Radiance.Core.Systems;
+
+namespace Radiance.Content.EncycloradiaEntries.Instruments
+{
+    public class GleamingWhetstoneEntry : EncycloradiaEntry
+    {
+        public GleamingWhetstoneEntry()
+        {
+            fastNavInput = "LDRR";
+            incomplete = UnlockCondition.UnlockedByDefault;
+            unlock = UnlockCondition.UnlockedByDefault;
+            category = EntryCategory.Instruments;
+            icon = ModContent.ItemType<GleamingWhetstone>();
+            visible = EntryVisibility.Visible;
+            
+            pages = [
+                new TextPage(),
+                //new TransmutationPage() 
+                //{ 
+                //    recipe = TransmutationRecipeSystem.FindRecipe(nameof(RingofFrugality)) 
+                //}
+            ];
+        }
+    }
+}
